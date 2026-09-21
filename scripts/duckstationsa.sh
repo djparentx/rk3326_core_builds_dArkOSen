d8684249b1d7cac81afcc54f827b2435488a4578
djparentx <276067176+djparentx@users.noreply.github.com>
2026-09-21 11:01:57 -0600

    add debug to Duckstation

--- scripts/duckstationsa.sh
+++ scripts/duckstationsa.sh
@@ -112,7 +112,7 @@ bitness="$(getconf LONG_BIT)"
 		     echo "There was an error that occured while making the duckstation standalone.  Stopping here."
              exit 1
            fi
-           strip bin/duckstation-nogui
+           # strip bin/duckstation-nogui  # TEMP: disabled for debug symbols, revert after gdb backtrace
 
            if [ ! -d "../../duckstationsa-$bitness/" ]; then
 		     mkdir -v ../../duckstationsa-$bitness
@@ -156,7 +156,7 @@ bitness="$(getconf LONG_BIT)"
 		     echo "There was an error that occured while making the duckstation standalone.  Stopping here."
              exit 1
            fi
-           strip bin/duckstation-nogui
+           # strip bin/duckstation-nogui  # TEMP: disabled for debug symbols, revert after gdb backtrace
 
            if [ ! -d "../../duckstationsa-$bitness/" ]; then
 		     mkdir -v ../../duckstationsa-$bitness
